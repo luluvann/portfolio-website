691e2b1e508226a7e33c4c10a0f07c0e0b9548d1
Lucile <developer.lvt@gmail.com>
2021-04-12 12:59:51 -0400

    added dynamic project card generation

--- script.js
+++ script.js
@@ -1,3 +1,30 @@
+var cards = [
+  {
+    title: "Weather Web App",
+    description: "A simple web app to see the current weather in a city",
+    technologies: "HTML, CSS, React, Node.js",
+    links: ["github","deployed"]
+  },
+  {
+    title: "Title",
+    description: "Description",
+    technologies: "Technologies",
+    links: ["github","deployed"]
+  },
+  {
+    title: "Title",
+    description: "Description",
+    technologies: "Technologies",
+    links: ["github","deployed"]
+  },
+  {
+    title: "Title",
+    description: "Description",
+    technologies: "Technologies",
+    links: ["github","deployed"]
+  },
+]
+
 $(window).resize(function(){
     if ($(window).width() > 1052) {
         $("nav").css("display","flex")
@@ -23,3 +50,24 @@ $(".nav-layer").on("click", function(){
   $(".nav-layer").css("display", "none");
   $("#navbar-toggler i").attr("class", "fas fa-bars")
 })
+
+function cardGenerator(cards){
+  for(var i = 0; i < cards.length ; i++){
+    console.log(cards)
+    var cardObj = cards[i]
+    $("#project-cards").append(`
+      <div class="card">
+        <h4><a>${cardObj.title}</a></h4>
+        <p>${cardObj.description}</p>
+        <h5>${cardObj.technologies}</h5>
+        <div class="links">
+          <i class="fab fa-github"><a href=${cardObj.links[1]}></a></i> 
+          <i class="fas fa-external-link-alt"><a href=${cardObj.links[2]}></a></i>
+        </div>
+      </div>
+  `)
+  }
+}
+
+cardGenerator(cards)
+console.log(cards)
