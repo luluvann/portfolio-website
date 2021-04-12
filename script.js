92c33d8855ebc065c35c1a25777dd7fe463d9d0d
Lucile <developer.lvt@gmail.com>
2021-04-12 15:32:41 -0400

    added style to cards

--- script.js
+++ script.js
@@ -2,35 +2,49 @@ var cards = [
   {
     title: "Weather Web App",
     description: "A simple web app to see the current weather in a city",
-    technologies: "HTML, CSS, React, Node.js",
-    links: ["github","deployed"]
+    technologies: "React, Open Weather Map API",
+    links: ["https://github.com/luluvann/react-weather-app","https://luluvann.github.io/react-weather-app/website/?city=Montreal"]
   },
   {
-    title: "Title",
-    description: "Description",
-    technologies: "Technologies",
-    links: ["github","deployed"]
+    title: "Github Repos Lister",
+    description: "List all the public repos of a given user",
+    technologies: "React, Github API",
+    links: ["https://github.com/luluvann/github-api","https://luluvann.github.io/github-api"]
   },
   {
-    title: "Title",
-    description: "Description",
-    technologies: "Technologies",
-    links: ["github","deployed"]
+    title: "Password Generator",
+    description: "Generate a random password based on criterias",
+    technologies: "Vanilla Javascript",
+    links: ["https://github.com/luluvann/password-generator","https://luluvann.github.io/password-generator"]
   },
   {
-    title: "Title",
-    description: "Description",
-    technologies: "Technologies",
-    links: ["github","deployed"]
+    title: "Mobile App Landing Page",
+    description: "A landing page to promote a mobile app",
+    technologies: "HTML, CSS",
+    links: ["github","https://www.pocontacts.app"]
+  },
+  {
+    title: "Javascript Code Quiz",
+    description: "A quiz to test coding knowledge",
+    technologies: "jQuery, Bootstrap 4",
+    links: ["https://github.com/luluvann/code-quiz","https://luluvann.github.io/code-quiz"]
+  },
+  {
+    title: "Pro-Trader Simulator",
+    description: "An app to simulate stock exchange trading (collaborative project)",
+    technologies: "jQuery, Bootstrap 4, Server side APIs",
+    links: ["https://github.com/luluvann/Pro-Trader","https://phonix375.github.io/Pro-Trader"]
   },
 ]
 
 $(window).resize(function(){
-    if ($(window).width() > 1052) {
+    if ($(window).width() >= 1052) {
+        console.log("equal or above 1052 "+$(window).width() )
         $("nav").css("display","flex")
         $(".nav-layer").css("display", "none");
         $("#navbar-toggler i").attr("class", "fas fa-bars")
-      } else if ($(window).width() <= 1052) {
+      } else if ($(window).width() < 1052) {
+        console.log("less thab 1052 "+$(window).width() )
         $("nav").css("display","none")
       }
   });
@@ -53,16 +67,15 @@ $(".nav-layer").on("click", function(){
 
 function cardGenerator(cards){
   for(var i = 0; i < cards.length ; i++){
-    console.log(cards)
     var cardObj = cards[i]
     $("#project-cards").append(`
       <div class="card">
-        <h4><a>${cardObj.title}</a></h4>
+        <h4><a href=${cardObj.links[1]}>${cardObj.title}</a></h4>
         <p>${cardObj.description}</p>
         <h5>${cardObj.technologies}</h5>
         <div class="links">
-          <i class="fab fa-github"><a href=${cardObj.links[1]}></a></i> 
-          <i class="fas fa-external-link-alt"><a href=${cardObj.links[2]}></a></i>
+          <a href=${cardObj.links[0]}><i class="fab fa-github"></i></a> 
+          <a href=${cardObj.links[1]}><i class="fas fa-external-link-alt"></i></a>
         </div>
       </div>
   `)
@@ -70,4 +83,3 @@ function cardGenerator(cards){
 }
 
 cardGenerator(cards)
-console.log(cards)
