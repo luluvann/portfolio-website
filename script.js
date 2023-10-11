2b46c1d134457ff6b8b63dab181005a7d8ca539c
lucile <developer.lvt@gmail.com>
2023-10-11 10:05:49 +0200

    formatted code

--- script.js
+++ script.js
@@ -3,43 +3,43 @@ var cards = [
     title: "Weather Web App",
     description: "A simple web app to see the current weather in a city",
     technologies: "React, Open Weather Map API",
-    links: ["https://github.com/luluvann/react-weather-app","https://luluvann.github.io/react-weather-app/website/?city=Montreal"]
+    links: ["https://github.com/luluvann/react-weather-app", "https://luluvann.github.io/react-weather-app/website/?city=Montreal"]
   },
   {
     title: "Github Repos Lister",
     description: "List all the public repos of a given user",
     technologies: "React, Github API",
-    links: ["https://github.com/luluvann/github-api","https://luluvann.github.io/github-api"]
+    links: ["https://github.com/luluvann/github-api", "https://luluvann.github.io/github-api"]
   },
   {
     title: "Password Generator",
     description: "Generate a random password based on criterias",
     technologies: "Vanilla Javascript",
-    links: ["https://github.com/luluvann/password-generator","https://luluvann.github.io/password-generator"]
+    links: ["https://github.com/luluvann/password-generator", "https://luluvann.github.io/password-generator"]
   },
   {
     title: "Mobile App Landing Page",
     description: "A landing page to promote a mobile app",
     technologies: "HTML, CSS",
-    links: ["https://github.com/luluvann/po-contacts","https://www.pocontacts.app"]
+    links: ["https://github.com/luluvann/po-contacts", "https://www.pocontacts.app"]
   },
   {
     title: "Javascript Code Quiz",
     description: "A quiz to test coding knowledge",
     technologies: "jQuery, Bootstrap 4",
-    links: ["https://github.com/luluvann/code-quiz","https://luluvann.github.io/code-quiz"]
+    links: ["https://github.com/luluvann/code-quiz", "https://luluvann.github.io/code-quiz"]
   },
   {
     title: "Weather Forecast Dashboard",
     description: "A simple weather dashboard to see current and weather forecast ",
     technologies: "jQuery, Bootstrap 4, Open Weather Map API",
-    links: ["https://github.com/luluvann/weather-dashboard","https://luluvann.github.io/weather-dashboard"]
+    links: ["https://github.com/luluvann/weather-dashboard", "https://luluvann.github.io/weather-dashboard"]
   },
   {
     title: "Pro-Trader Simulator",
     description: "An app to simulate stock exchange trading (collaborative project)",
     technologies: "jQuery, Bootstrap 4, Server side APIs",
-    links: ["https://github.com/luluvann/Pro-Trader","https://phonix375.github.io/Pro-Trader"]
+    links: ["https://github.com/luluvann/Pro-Trader", "https://phonix375.github.io/Pro-Trader"]
   },
   {
     title: "Live The Outdoors",
@@ -55,15 +55,15 @@ var cards = [
   },
 ]
 
-$(window).resize(function(){
-    if ($(window).width() >= 1052) {
-        $("nav").css("display","flex")
-        $(".nav-layer").css("display", "none");
-        $("#navbar-toggler i").attr("class", "fas fa-bars")
-      } else if ($(window).width() < 1052) {
-        $("nav").css("display","none")
-      }
-  });
+$(window).resize(function () {
+  if ($(window).width() >= 1052) {
+    $("nav").css("display", "flex")
+    $(".nav-layer").css("display", "none");
+    $("#navbar-toggler i").attr("class", "fas fa-bars")
+  } else if ($(window).width() < 1052) {
+    $("nav").css("display", "none")
+  }
+});
 
 $("#navbar-toggler").on("click", "i", function () {
   if ($(this).attr("class") == "fas fa-bars") {
@@ -75,7 +75,7 @@ $("#navbar-toggler").on("click", "i", function () {
   }
 });
 
-$(".nav-layer").on("click", function(){
+$(".nav-layer").on("click", function () {
   $(".nav-layer").css("display", "none");
   $("#navbar-toggler i").attr("class", "fas fa-bars")
 })
